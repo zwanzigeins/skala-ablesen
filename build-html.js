dc1e30aba3cbe6c0159366eab321e5b0b5543496
Lukas Glowania <lukas.glowania@justedit.com>
2022-02-23 12:54:00 +0100

    remove body-padding, set svg-width to 100%

--- build-html.js
+++ build-html.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 let src = 'src/index.html';
 let dest = 'out/index.html';
 
-// File destination.txt will be created or overwritten by default.
 fs.copyFile(src, dest, (err) => {
   if (err) throw err;
   console.log(src + ' was copied to ' + dest);
